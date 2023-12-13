25a9d7df7ef4eb2c3ed8816d6406d5c6742ba964
Georgy howl <howluwqz1@gmail.com>
2023-12-13 15:24:38 +0300

    improve 'Option'

--- middle/option/main.c
+++ middle/option/main.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-typedef struct {
-    void* value;
-    int has_value;
-} Option;
-
-
-const Option None = {NULL, 0};
+typedef struct { void* value; } Option;
+const Option None = { NULL };
 
 
 Option Some(void* value) {
-    Option option = {value, 1};
+    Option option = { value };
     return option;
 }
 
 
 Option map(Option option, void* (*func)(void*)) {
-    if (option.has_value)
+    if (option.has_value != NULL)
         return Some(func(option.value));
     return None;
 }
 
 
 void *unwrap(Option option) {
-    if (option.has_value) return option.value;
+    if (option.has_value != NULL) return option.value;
     fprintf(stderr, "Cannot unwrap None.\n");
     exit(EXIT_FAILURE);
 }
